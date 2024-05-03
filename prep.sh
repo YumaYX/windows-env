a4a27861a4aee42824285b1c45663b7fbfbcc246
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-03 19:02:28 +0000

    update

--- prep.sh
+++ prep.sh
@@ -3,13 +3,13 @@
 cat <<'CONFIGEOF' > ~/.ssh/config
 Host GitHub
     HostName github.com
-    IdentityFile /vagrant/id_rsa
+    IdentityFile ~/yuma/.ssh/id_rsa
     TCPKeepAlive yes
     IdentitiesOnly yes
     User git
 CONFIGEOF
 
-cp -v /vagrant/id_rsa ~/.ssh/
+cp -fv /vagrant/id_rsa ~/.ssh/
 chown yuma. ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 
