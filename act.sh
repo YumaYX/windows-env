c7196bb9852c1c8ecb5d4020a3a3790b4add6a45
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-04 03:52:31 +0000

    update

---
+++ act.sh
@@ -0,0 +1,6 @@
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl --now enable docker
+sudo usermod -a -G docker yuma
+
+cd && curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
